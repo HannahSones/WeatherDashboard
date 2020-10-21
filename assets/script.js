e6d3987e63de96283ca4f46033581ef001158d40
Hannah Sones <hannah.sones@outlook.com>
2020-10-21 16:19:58 +0100

    Save button pulls correct data from the Openweather API and displays city name and date at the top

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,52 @@
+const date = moment().format("LL");
+const searchedCity = $("#citySearch input[name='city']");
+const cityName = searchedCity.val();
+
+const APIKey = "f9de6cd0ea9d859bd4d2804e8ab205c3";
+
+
+const currentWeatherData = $("div.currentForecast");
+console.log("City", searchedCity, currentWeatherData);
+
+
+// search the API using city name
+$("#searchBtn").click(function searchWeather() {
+    console.log("Search button pressed");
+    console.log("Searched city", searchedCity.val());
+    const cityName = searchedCity.val();
+    const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+    console.log(queryURL);
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (weatherData) {
+        updateResults(weatherData);
+        $("#errorMsg").hide();
+    }).catch(function (error) {
+        console.log("whoops, error");
+        $("#errorMsg").show();
+    });
+
+    function updateResults(weatherData) {
+        console.log(weatherData)
+        
+        $("#cityName").text(cityName + ' (' + date + ') ');
+
+        // const currentWeatherIcon = results.weather.icon;
+        // const currentTemp = results.main.temp;
+        // const currentHumidity = results.main.humidity;
+        // const currentWind = results.wind.speed;
+        // // const currentUVIndex = 
+        // for ()
+
+        // currentWeatherData.append
+
+
+    // localStorage.setItem(searchedCity, weatherData);
+
+}});
+
+
 $(document).ready(function () {
 
 
@@ -7,19 +56,18 @@ $(document).ready(function () {
     // When clear search history button is pressed, it clears all data from local storage and refreshes the page
 
     $("#clearSearchHistory").on("click", function () {
-            console.log("Clear history button pressed")
-            window.localStorage.clear();
-            window.location.reload();
-        });
+        window.localStorage.clear();
+        window.location.reload();
+    });
 
 
 });
 
 
-
 // psuedo-code
-// 1. Users can search for city in the search box
-// 2. The name of the city and date display
+// 1. Users can search for city in the search box -
+// 2. An error message appears if the city is not recognised -
+// 2. The name of the city and date display -
 // 3. Todays temp, icon, humidity, wind, UV display in the box
 // 4. The five day forecast is populated
 // 5. Searched cities appear in a list of previously searched for
